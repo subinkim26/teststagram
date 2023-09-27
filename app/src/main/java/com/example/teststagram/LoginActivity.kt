8a7dbdf5198627d377177e61b580cbc5651ef997
김수빈 <77378559+subinkim26@users.noreply.github.com>
2023-09-27 14:09:30 +0900

    email and password 회원가입 및 이메일 인증
    이메일과 비밀번호를 이용하여 회원가입 및 이메일 인증 완료
    
    Ref : 비밀번호 & 아이디 찾기 기능

--- app/src/main/java/com/example/teststagram/LoginActivity.kt
+++ app/src/main/java/com/example/teststagram/LoginActivity.kt
@@ -1,11 +1,67 @@
 package com.example.teststagram
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.teststagram.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : AppCompatActivity() {
+
+
+    lateinit var auth : FirebaseAuth
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
+
+        binding.emailLoginButton.setOnClickListener {
+            siginAndSignup()
+        }
+
+    }
+
+
+    fun siginAndSignup(){
+        var id = binding.edittextId.text.toString()
+        var password = binding.edittextPassword.text.toString()
+        auth.createUserWithEmailAndPassword(id, password).addOnCompleteListener {
+            task ->
+            if(task.isSuccessful){
+                //아이디 생성 -> 메인화면 이동
+                moveMain(task.result?.user)
+            }else{
+                //아이디 중복의 경우?'
+                siginEmail()
+
+            }
+        }
+
+    }
+
+    fun moveMain(user : FirebaseUser?){
+        if(user != null){
+            if(user.isEmailVerified){
+                finish()//로그인 화면 종료
+                startActivity(Intent(this,MainActivity::class.java))
+            }else{
+                user.sendEmailVerification()
+            }
+
+        }
+    }
+
+    fun siginEmail(){
+        var id = binding.edittextId.text.toString()
+        var password = binding.edittextPassword.text.toString()
+        auth.signInWithEmailAndPassword(id,password).addOnCompleteListener {
+            task->
+            if(task.isSuccessful){
+                moveMain(task.result?.user)
+            }
+        }
     }
 }
